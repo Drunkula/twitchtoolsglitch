c6c35572ef71b26f87ef9670bda82b8788e3d2e6
Drunkula <spudguzzler@sharklasers.com>
2024-04-06 17:34:07 +0100

    auto ratio non perfect 4:3 compensation

--- ytplayer/ytplayer.autoratio.mod.js
+++ ytplayer/ytplayer.autoratio.mod.js
@@ -2,6 +2,7 @@
  * Resize the player to 4:3 size when a real 4:3 video is present
  */
 var playerContainer = document.getElementById("ytcontainer");
+var ytIframe;
 
 // what events does the YT internal player have?  onStateChange, that'll do
 // https://developers.google.com/youtube/iframe_api_reference#Events
@@ -10,6 +11,7 @@ export async function auto_ratio_init() {
     ytcontainer.classList.add("youtube-player");
     await window.ytpc.ytPlayer.ytPlayerReady();
     ytpc.ytPlayer.addPlayerListener('onStateChange', auto_ratio_handler);
+    ytIframe = document.getElementById("ytplayer");
 }
 
     // Playing seems to be the only reliable state to change ratio on.
@@ -20,14 +22,17 @@ export function auto_ratio_handler(p) {
 
     if (p.data === 1) // 1 = playing
     if (ratio  - 0.65 > 0) { // will be 0.75 or .5625, do a 'trick'
-        playerContainer.classList.add("youtube-player-43");
+        ytplayer.style.width = `${56.25 / ratio}%`;
+        //playerContainer.classList.add("youtube-player-43");
+        //ytplayer.style.width="75%";
     } else {
-        playerContainer.classList.remove("youtube-player-43");
+        //playerContainer.classList.remove("youtube-player-43");
+        ytplayer.style.width="100%";
     }
 }
 /*
-    let wideOr43 = (ratio - 0.6 > 0) ? "4:3" : "Widescreeen";
-    console.log("STATE CHANGE", p);
-    console.log("STATE: ", p.data, ytpc.ytPlayer.states[p.data], "for:", p.target.videoTitle);
-    console.log(`Ratio: ${vr.height}/${vr.width} = ${vr.height/vr.width} guess: ${wideOr43}`);
+let wideOr43 = (ratio - 0.6 > 0) ? "4:3" : "Widescreeen";
+console.log("STATE CHANGE", p);
+console.log("STATE: ", p.data, ytpc.ytPlayer.states[p.data], "for:", p.target.videoTitle);
+console.log(`Ratio: ${vr.height}/${vr.width} = ${vr.height/vr.width} guess: ${wideOr43}`);
 */
